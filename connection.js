e8b3c82a70fb9bb08bdf3d48460eb34c3e4a4125
Jashwanth Reddy <126757471+jashwanth-rdy@users.noreply.github.com>
2023-12-06 23:22:13 +0530

    update password

--- connection.js
+++ connection.js
@@ -4,7 +4,7 @@ const db = mysql.createConnection({
   host: "localhost",
   user: "root",
   database: "dbms",
-  password: "ApJaRe@mysql#1947",
+  password: "password",
 });
 
 module.exports = db;
